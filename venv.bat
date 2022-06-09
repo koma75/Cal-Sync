671485296e07f09c662f4879a29bf149c7052d01
koma75 <okunoya@path-works.net>
2022-06-09 19:11:53 +0900

    remove venv initialization batch file

--- venv.bat
+++
@@ -1,6 +0,0 @@
-title Cal Sync
-REM set HTTP_PROXY=http://localhost:8089
-REM set HTTPS_PROXY=http://localhost:8089
-if not exist .venv python -m venv .venv
-REM start D:\Programs\Tools\yappa\yappa.exe
-cmd /k .venv\Scripts\activate.bat
